9ddd9ea90af259b828ee9c5880806344c7223aa8
Niladri <niladrisengit12@gmail.com>
2023-09-03 22:23:40 +0530

    day-03

--- proj2/index.js
+++ proj2/index.js
@@ -12,7 +12,21 @@ const resolvers = {
     game :(parent,args) => db.games.find(game=>game?.id === args?.id),
     review :(parent,args) => db.reviews.find(review=>review?.id === args?.id),
     author :(parent,args) => db.authors.find(author=>author?.id === args?.id)
-  }
+  },
+  Game:{
+    //ekhane within reviews parent is a single game
+    reviews :(parent) => db?.reviews?.filter(review => review?.game_id === parent?.id)
+  },
+  Review:{
+    //ekhane within author parent is a single review
+      author:(parent) => db?.authors?.find(author=>author?.id === parent.author_id),
+      game:(parent) => db?.games?.find(game=>game?.id === parent?.game_id),
+    },
+    Author:{
+      //ekhane within review parent is a single author
+      reviews :(parent) => db?.reviews?.filter(review => review?.author_id === parent?.id)
+    },
+
 }
 
 const server = new ApolloServer({
